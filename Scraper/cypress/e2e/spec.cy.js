c5b415af2093a20b941f5b08f2fc7974495c280b
Siddhant Singh <singhsiddhantakshat@gmail.com>
2023-07-13 10:47:17 -0500

    changes...

--- Scraper/cypress/e2e/spec.cy.js
+++ Scraper/cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+require("dotenv").config();
+
+describe("template spec", () => {
+	it("passes", () => {
+		cy.visit("https://example.cypress.io");
+		console.log(process.env.email);
+	});
+});
